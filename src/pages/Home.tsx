997362f002efd3d93ea9952d60a791044aba5390
sichan1301 <sichan1301@naver.com>
2025-07-11 17:00:48 +0900

    Home Page UI 구현

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,17 @@
+import Button from "../components/Button"
+import DiaryList from "../components/DiaryList"
+import Header from "../components/Header"
 
 const Home = () => {
-
 	return (			
 		<>
-			Home
+			<Header
+				title = {"2025.07"} 
+				leftChild = {<Button text="<" type="Default" />} 
+				rightChild = {<Button text=">" type="Default" />} 
+			/>
+
+			<DiaryList />
 		</>
 
 	)
