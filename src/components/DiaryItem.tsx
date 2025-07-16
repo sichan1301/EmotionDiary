3f38ceb8f81741aaab8b3e2739c8b3a3115d3832
sichan1301 <sichan1301@naver.com>
2025-07-16 19:11:50 +0900

    홈화면 기능

--- src/components/DiaryItem.tsx
+++ src/components/DiaryItem.tsx
@@ -1,3 +1,4 @@
+import { useNavigate } from "react-router-dom";
 import { getEmotionImage } from "../util/get-emotion-image"
 import Button from "./Button"
 import "./DiaryItem.css";
@@ -10,22 +11,31 @@ interface DiaryItemProps{
   content: string;
 }
 const DiaryItem = ({id,createdDate,emotionId,content}:DiaryItemProps) => {
+  const nav = useNavigate();
+
+  const goToDetailPage = () => {
+    nav(`/diary/${id}`)
+  }
+
+  const goToEditPage = () => {
+    nav(`/edit/${id}`)
+  }
 
   return(
     <div className="DiaryItem">
 
-      <div className={`img_section img_section_${emotionId}`}>
+      <div className={`img_section img_section_${emotionId}`} onClick = {goToDetailPage}>
         <img src={getEmotionImage(emotionId)}  />
       </div>
 
-      <div className="info_section">
+      <div className="info_section" onClick = {goToDetailPage}>
         <div className="created_date">
           {new Date(createdDate).toLocaleDateString()}
         </div>
         <div className="content">{content}</div>
       </div>
 
-      <div className="button_section">
+      <div className="button_section" onClick = {goToEditPage}>
         <Button text="수정하기" type="Default" />
       </div>
     </div>
--- src/components/DiaryList.tsx
+++ src/components/DiaryList.tsx
@@ -1,25 +1,55 @@
+import { useNavigate } from "react-router-dom";
 import type { mockDataType } from "../mockData";
 import Button from "./Button";
 import DiaryItem from "./DiaryItem";
 import "./DiaryList.css";
+import { useCallback, useEffect, useState } from "react";
 
 interface DiaryListProps {
   data:mockDataType[]
 }
 
 const DiaryList = ({data}:DiaryListProps) => {
+  const [currentSort, setCurrentSort] = useState("latest")
+
+  const updateSorted = (e:React.ChangeEvent<HTMLSelectElement>) => {
+    setCurrentSort(e.target.value)
+  }
+
+  const getSortedData = useCallback(() => {
+    return data.slice().sort((a,b)=>{
+      if(currentSort === "latest"){
+        return b.createdDate - a.createdDate
+      }else{
+        return a.createdDate - b.createdDate
+      }
+    })
+  },[currentSort])
+
+  const nav = useNavigate();
+
+  const goToNewPage = () => {
+    nav('/new');
+  }
+
+
+  useEffect(()=>{
+    console.log(currentSort);
+  },[currentSort])
+
+
   return(
     <div className="DiaryList">
       <div className="menu_bar">
-        <select>
+        <select onChange = {updateSorted}>
           <option value="latest">최신순</option>
           <option value="oldest">오래된 순</option>
         </select>
-        <Button text="새 일기 쓰기" type = "Positive"/>
+        <Button onClick = {goToNewPage} text="새 일기 쓰기" type = "Positive"/>
       </div>
       
       <div className="list_wrapper">
-        {data.map((item:mockDataType) => 
+        {getSortedData().map((item:mockDataType) => 
           <DiaryItem key= {item.id} {...item}/>
         )}
       </div>
